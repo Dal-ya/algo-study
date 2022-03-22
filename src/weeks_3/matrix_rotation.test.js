ced6f999f49341cd90cbfbd9c36de44e26e4769c
Dal-ya <hyansky@daum.net>
2022-03-22 21:07:31 +0900

    Add | idea

---
+++ src/weeks_3/matrix_rotation.test.js
@@ -0,0 +1,12 @@
+const solution = require('./lotto');
+
+describe('행렬 테두리 회전하기', () => {
+  test('solution test 1', () => {
+    const queries = [
+      [2, 2, 5, 4],
+      [3, 3, 6, 6],
+      [5, 1, 6, 3],
+    ];
+    expect(solution(6, 6, queries).toEqual([8, 10, 25]));
+  });
+});
