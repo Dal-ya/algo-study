995fdea3e1176877f4d86b189be689a125561834
Dal-ya <hyanskygg@gmail.com>
2022-03-31 22:52:36 +0900

    solution

--- src/weeks_4/ranking_search.js
+++ src/weeks_4/ranking_search.js
@@ -7,11 +7,10 @@ const info = [
   'cpp backend senior pizza 260',
   'java backend junior chicken 80',
   'python backend senior chicken 50',
-  'java backend junior chicken 150',
 ];
 
 const query = [
-  'java and backend and junior and - 100',
+  'java and backend and junior and pizza 100',
   'python and frontend and senior and chicken 200',
   'cpp and - and senior and pizza 250',
   '- and backend and senior and - 150',
@@ -29,43 +28,48 @@ function solution(info, query) {
   const splitInfo = splitSpaceAnd(info);
   const splitQuery = splitSpaceAnd(query);
 
-  console.log('==splitInfo====');
-  console.log(splitInfo);
-  console.log('==splitQuery====');
-  console.log(splitQuery);
+  // console.log('==splitInfo====');
+  // console.log(splitInfo);
+  // console.log('==splitQuery====');
+  // console.log(splitQuery);
 
-  const q = splitQuery[0]; // 한줄만 테스트
-  const lastIdx = q.length - 1;
+  for (let i = 0; i < splitQuery.length; i++) {
+    const q = splitQuery[i];
+    const lastIdx = q.length - 1;
 
-  let resultArr = [];
-  function check(list, idx) {
-    const r = list.filter((arr) => {
-      if (q[idx] === '-') {
-        return list;
-      }
-      return arr[idx] === q[idx] || Number(arr[idx]) >= Number(q[idx]);
-    });
-
-    if (idx < lastIdx) {
-      idx++;
-      check(r, idx);
-    } else {
-      resultArr = [...r];
-    }
-    // resultArr = [...r];
-
-    // console.log('resultArr: ', resultArr);
-    return resultArr;
+    const findArr = filterling(q, lastIdx, splitInfo);
+    // console.log('findArr: ', findArr);
+    answer.push(findArr.length);
+    // console.log('answer: ', answer);
   }
 
-  console.log('----------------');
-  const rrr = check(splitInfo, 0);
-  console.log('rrr: ', rrr);
-  // console.log(check(splitInfo, 0));
-  // console.log(check(check(splitInfo, 0), 1));
-  // console.log(check(check(check(splitInfo, 0), 1), 2));
+  function filterling(q, lastIdx, splitInfo) {
+    let resultArr = [];
+
+    // 재귀 함수
+    function check(list, idx) {
+      const r = list.filter((arr) => {
+        if (q[idx] === '-') {
+          return list;
+        }
+        return arr[idx] === q[idx] || Number(arr[idx]) >= Number(q[idx]);
+      });
+
+      if (idx < lastIdx) {
+        idx++;
+        check(r, idx);
+      } else {
+        resultArr = [...r];
+      }
+    }
+
+    check(splitInfo, 0);
+
+    return resultArr;
+  }
 
   return answer;
 }
 
-solution(info, query);
+const r = solution(info, query);
+console.log(r);
