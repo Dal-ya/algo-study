2aa0dda1f0e8583a46ccb45d25402516b93966f7
Dal-ya <hyansky@daum.net>
2022-05-17 20:04:24 +0900

    Add | 설명 주석

--- src/weeks_11/prefix_sum.js
+++ src/weeks_11/prefix_sum.js
@@ -5,6 +5,11 @@
 // R: Row, C: Column
 // S[R + 1][C + 1] =  S[R + 1][C] + S[R][C + 1] - S[R][C] + A[R][C]
 
+// https://gamedevlog.tistory.com/68
+// Prefix sum은 누적 합을 미리 구하는 전처리 과정을 통해 구간 합(range sum)을 빠르게 구할 때 사용된다.
+// prefix sum : 0~b까지의 누적합 (반드시 첫번 째 원소를 포함하는 구간)
+// range sum : a~b까지의 구간 합
+
 const A = [
   [3, 1, 5, 7, 2],
   [9, 4, 8, 9, 7],
@@ -15,18 +20,22 @@ const A = [
 ];
 
 function makePrefixSum(A) {
-  let S = JSON.parse(JSON.stringify(A));
+  let S = JSON.parse(JSON.stringify(A)); // 배열 값 복사
 
   S.unshift(new Array(A[0].length + 1).fill(0));
   for (let i = 1; i < S.length; i++) {
     S[i].unshift(0);
   }
 
+  console.log(S);
+
   for (let i = 0; i < A.length; i++) {
     for (let j = 0; j < A[i].length; j++) {
       S[i + 1][j + 1] = S[i + 1][j] + S[i][j + 1] - S[i][j] + A[i][j];
     }
   }
+
+  console.log(S);
 
   S.shift();
 
