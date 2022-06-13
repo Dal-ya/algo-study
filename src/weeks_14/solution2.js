ea1e11c539360a17866de537200669e1089d529d
Dal-ya <hyansky@daum.net>
2022-06-14 08:13:42 +0900

    Add weeks 14

--- src/weeks_14/solution2.js
+++ src/weeks_14/solution2.js
@@ -1,7 +1,53 @@
 function solution(begin, end) {
   var answer = [];
 
-  const lastBlcok = end / 2;
+  for (let i = begin; i <= end; i++) {
+    if (i === 1) {
+      answer.push(0);
+      continue;
+    }
+
+    answer.push(maxDivisor(i));
+  }
 
   return answer;
 }
+
+function isPrime(num) {
+  if (num === 1) {
+    return false;
+  }
+
+  if (num === 2) {
+    return true;
+  }
+
+  const n = parseInt(Math.sqrt(num));
+
+  for (let i = 2; i <= n; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function maxDivisor(num) {
+  if (num === 1) {
+    return 1;
+  }
+
+  if (isPrime(num)) {
+    return 1;
+  }
+
+  for (let i = 2; i <= num; i++) {
+    if (num % i === 0) {
+      return num / i;
+    }
+  }
+}
+
+const r = solution(1, 10);
+console.log(r);
