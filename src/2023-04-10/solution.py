69c10982920d0167f00ee4b8eb195a46f568c1c1
Dal-ya <hyanskygg@gmail.com>
2023-04-10 09:22:46 +0900

    Add | pyhton 풀이 추가

--- src/2023-04-10/solution.py
+++ src/2023-04-10/solution.py
@@ -4,14 +4,24 @@ from collections import Counter
 
 def solution(k, tangerine):
     answer = 0
+    tempList = []
+    tangCounter = Counter(tangerine)
+    for key, val in tangCounter.items():
+        tempList.append([key, val])
 
-    a = Counter(tangerine)
-    b = a.items();
-    print(b)
-    
+    # 2차원 배열 sort
+    # https://haesoo9410.tistory.com/193
+    tempList.sort(key=lambda x: (x[1], -x[1]), reverse=True)
+    # print(tempList)
 
+    for i in range(len(tempList)):
+        # print(k)
+        k = k - tempList[i][1]
+        if k <= 0:
+            answer = i + 1
+            break
 
-
+    # print(answer)
     return answer
 
-solution(2, [1,1,1,2,2,5,5,5,5])
+solution(6, [1, 3, 2, 5, 4, 5, 2, 3]);
